76fb129f6f49c6c977392e1c571dbb5b1e7778ba
Member1221 <clipseypone@gmail.com>
2020-10-09 22:57:57 +0200

    Reference meshdata in inochi2d.render

--- source/inochi2d/render/package.d
+++ source/inochi2d/render/package.d
@@ -11,3 +11,4 @@ module inochi2d.render;
 public import inochi2d.render.shader;
 public import inochi2d.render.texture;
 public import inochi2d.render.dynmesh;
+public import inochi2d.render.meshdata;
