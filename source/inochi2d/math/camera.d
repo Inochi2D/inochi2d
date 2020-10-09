e50f6dd82199323a00733418e4e2ce7a2dac97b1
Member1221 <clipseypone@gmail.com>
2020-10-09 17:53:07 +0200

    Add camera class

---
+++ source/inochi2d/math/camera.d
@@ -0,0 +1,41 @@
+/*
+    Inochi2D Real-time puppet
+
+    Copyright © 2020, Inochi2D Project
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+    
+    Authors: Luna Nielsen
+*/
+module inochi2d.math.camera;
+import inochi2d.math;
+
+/**
+    An orthographic camera
+*/
+class Camera {
+private:
+    mat4 projection;
+
+public:
+
+    this() {
+        position = vec2(0, 0);
+    }
+
+    /**
+        Position of camera
+    */
+    vec2 position;
+
+    /**
+        Matrix for this camera
+
+        width = width of camera area
+        height = height of camera area
+    */
+    mat4 matrix(int width, int height) {
+        return 
+            mat4.orthographic(0f, cast(float)width, cast(float)height, 0, 0, max(width, height)) * 
+            mat4.translation(position.x, position.y, -2);
+    }
+}
