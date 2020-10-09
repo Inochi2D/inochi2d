d0e098e74bfba4055031ff937d5d413a2f4455bb
Member1221 <clipseypone@gmail.com>
2020-10-09 16:44:27 +0200

    Add OpenGL dep and render module

---
+++ source/inochi2d/render/package.d
@@ -0,0 +1,12 @@
+/*
+    Inochi2D Rendering
+
+    Copyright © 2020, Inochi2D Project
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+    
+    Authors: Luna Nielsen
+*/
+module inochi2d.render;
+
+public import inochi2d.render.shader;
+public import inochi2d.render.texture;
