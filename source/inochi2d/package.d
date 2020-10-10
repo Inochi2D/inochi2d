9f15b9f2fa2797900484056ebf128210dec64e9b
Member1221 <clipseypone@gmail.com>
2020-10-10 04:50:39 +0200

    Add delta time managment to inochi2d module

--- source/inochi2d/package.d
+++ source/inochi2d/package.d
@@ -5,16 +5,52 @@
     Authors: Luna Nielsen
 */
 module inochi2d;
-public import inochi2d.puppet;
+public import inochi2d.inochi2d;
 public import inochi2d.math;
 public import inochi2d.phys;
 public import inochi2d.fmt;
 public import inochi2d.render;
 
+private double currentTime_;
+private double lastTime_;
+private double deltaTime_;
+private double function() tfunc_;
+
 /**
     Initializes Inochi2D
     Run this after OpenGL context has been set current
 */
-void initInochi2D() {
+void initInochi2D(double function() timeFunc) {
     initDynMesh();
+    tfunc_ = timeFunc;
+}
+
+/**
+    Run this at the start of your render/game loop
+*/
+void updateInochi2D() {
+    currentTime_ = tfunc_();
+    deltaTime_ = currentTime_-lastTime_;
+    lastTime_ = currentTime_;
+}
+
+/**
+    Gets the time difference between the last frame and the current frame
+*/
+double deltaTime() {
+    return deltaTime_;
+}
+
+/**
+    Gets the last frame's time step
+*/
+double lastTime() {
+    return lastTime_;
+}
+
+/**
+    Gets the current time step
+*/
+double currentTime() {
+    return currentTime_;
 }
