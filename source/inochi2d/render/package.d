70f29dc62168c8a925256ee38d7cfb53769fe10f
Member1221 <clipseypone@gmail.com>
2020-12-06 08:21:28 +0100

    Add inochi2d.render rendering constants and utils

--- source/inochi2d/render/package.d
+++ source/inochi2d/render/package.d
@@ -11,3 +11,38 @@ module inochi2d.render;
 public import inochi2d.render.shader;
 public import inochi2d.render.texture;
 public import inochi2d.render.mesh;
+
+import bindbc.opengl;
+import inochi2d.math;
+
+// Internal rendering constants
+package(render) {
+    int inViewportWidth;
+    int inViewportHeight;
+
+    Camera inCamera;
+}
+
+/**
+    Sets the global camera
+*/
+void inSetCamera(Camera camera) {
+    inCamera = camera;
+}
+
+/**
+    Sets the viewport area to render to
+*/
+void inSetViewport(int width, int height) {
+    glViewport(0, 0, width, height);
+    inViewportWidth = width;
+    inViewportHeight = height;
+}
+
+/**
+    Gets the viewport
+*/
+void inGetViewport(out int width, out int height) {
+    width = inViewportWidth;
+    height = inViewportHeight;
+}
