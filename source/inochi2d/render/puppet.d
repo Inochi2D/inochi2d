f2c90ddd98e1289a123179c2536e519dc5b37ca6
Member1221 <clipseypone@gmail.com>
2020-12-18 00:21:02 +0100

    TLMesh

---
+++ source/inochi2d/render/puppet.d
@@ -0,0 +1,30 @@
+/*
+    Copyright © 2020, Inochi2D Project
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+    
+    Authors: Luna Nielsen
+*/
+module inochi2d.render.puppet;
+import inochi2d.render.mesh;
+
+/**
+    TopLevel Mesh
+*/
+struct TLMesh {
+    
+    /**
+        value from -1 to 1 which determines sort order
+
+        Sorting is done from back to front, -1 is in back, 1 is in front
+    */
+    float z;
+
+    /**
+        Reference to the mesh itself
+    */
+    DynMesh mesh;
+}
+
+class Puppet {
+    TLMesh[] meshes;
+}
