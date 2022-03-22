12205c572acff4861f2fd22f265ad5de442f9b29
LunaTheFoxgirl <cli-luna@protonmail.com>
2022-03-22 12:40:01 +0100

    Add areLineSegmentsIntersecting to math module

--- source/inochi2d/math/package.d
+++ source/inochi2d/math/package.d
@@ -48,3 +48,16 @@ bool contains(vec4 a, vec2 b) {
             b.x <= a.x+a.z &&
             b.y <= a.y+a.w;
 }
+
+/**
+    Checks if 2 lines segments are intersecting
+*/
+bool areLineSegmentsIntersecting(vec2 p1, vec2 p2, vec2 p3, vec2 p4) {
+    float epsilon = 0.00001f;
+    float demoninator = (p4.y - p3.y) * (p2.x - p1.x) - (p4.x - p3.x) * (p2.y - p1.y);
+    if (demoninator == 0) return false;
+
+    float uA = ((p4.x - p3.x) * (p1.y - p3.y) - (p4.y - p3.y) * (p1.x - p3.x)) / demoninator;
+    float uB = ((p2.x - p1.x) * (p1.y - p3.y) - (p2.y - p1.y) * (p1.x - p3.x)) / demoninator;
+    return (uA > 0+epsilon && uA < 1-epsilon && uB > 0+epsilon && uB < 1-epsilon);
+}
